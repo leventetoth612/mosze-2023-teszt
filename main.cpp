80e9fd85da1edc8d21c2630d44a8381a72728c2f
Toth Levente <leventetoth612@gmail.com>
2026-03-01 15:07:32 +0100

    hibak javitva

--- main.cpp
+++ main.cpp
@@ -4,28 +4,20 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // int *b = new int[NELEMENTS]; // Hiba: NELEMENTS helyett N_ELEMENTS
     int *b = new int[N_ELEMENTS];
-    // std::cout << '1-100 ertekek duplazasa' // Hiba: hiányzik << std::endl; és idézőjel helyett aposztróf
     std::cout << "1-100 ertekek duplazasa" << std::endl;
-    // for (int i = 0;) // Hiba: hiányzik a feltétel és az inkrementálás
     for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    // for (int i = 0; i; i++) // Hiba: feltétel helyesen i < N_ELEMENTS kell
     for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // std::cout << "Ertek:" // Hiba: hiányzik << b[i] és a pontosvessző
         std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    // int atlag; // Hiba: nincs inicializálva
     int atlag = 0;
-    // for (int i = 0; i < N_ELEMENTS, i++) // Hiba: vessző helyett pontosvessző kell
     for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // atlag += b[i] // Hiba: hiányzik a pontosvessző
         atlag += b[i];
     }
     atlag /= N_ELEMENTS;
